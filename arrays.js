ed4cdf26f67b27f13af924fb2acaad91ac9b772b
Pitta <mailez4all@gmail.com>
2018-02-03 09:12:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -57,3 +57,10 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 
 }
+
+function removeElementFromEndOfArray(array) {
+  
+  var array =[1,2,3]
+  array = array.slice(0, array.length - 1)
+  return array
+}
