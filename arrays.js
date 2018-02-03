2a8120a7e76ca7f6b8309713265f1f22b90deaa7
Pitta <mailez4all@gmail.com>
2018-02-03 08:50:37 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,38 @@
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+
+
+function addElementToBeginningOfArray(array, element) {
+
+  array = ["foo", ...array]
+  return array
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+  array.unshift("foo")
+  return array
+
+}
+
+function addElementToEndOfArray(array, element) {
+
+  array = [...array, "foo"]
+  return array
+
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+
+  array.push("foo")
+  return array
+
+}
+
+function accessElementInArray(array) {
+
+  var array = [3, "bye", "go"]
+  return array[0]
+
+}
