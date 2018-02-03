d140077823d7c39844d93560b372d52cdee1c1c6
Pitta <mailez4all@gmail.com>
2018-02-03 09:23:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function accessElementInArray(array) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   var array = [ 1, 2, 3 ]
-  array.shift()
+  array = array.shift()
   return array
 }
 
